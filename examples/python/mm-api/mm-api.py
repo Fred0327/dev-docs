d85f3b5f9ee572385fc7f3dc0cabd6ab9fb4a235
Fred <fred@zklink.org>
2024-01-11 17:44:06 +0800

    fix place order error

--- examples/python/mm-api/mm-api.py
+++ examples/python/mm-api/mm-api.py
@@ -8,17 +8,17 @@ import hashlib
 import hmac
 import zklink_sdk as sdk
 
-domain_url = "http://domain_url"
-eth_priv_key = ""
-api_key = "your api key"
-api_secret = "your api secret"
+domain_url = "http://8.217.46.106:8020/bn-v1"
+eth_priv_key = "80bca4596b18cc2d7af5580a28d9c43b57873453f1e2517e893bc0468d158c33"
+api_key = "db0d1ceb8788477e76ff6882093930a8a0f8bf5c49fb9a286f05b6c9be390fe8"
+api_secret = "7d9eae845ad409bdb5dc47c12baf99e0339c6d1a8e5b2466019b4978742f7969"
 
 
 def server_info():
     path = "/mm/api/server"
     url = "%s%s" % (domain_url, path)
     resp = requests.get(url)
-    print(resp.json())
+    print(resp)
     return resp.json()
 
 
@@ -91,7 +91,7 @@ def get_slot_batchly(cnt):
     return resp.json()
 
 
-def place_order(acct_id, product, side, time_in_force, price, size,
+def place_order(account_id, product, side, time_in_force, price, size,
                 taker_fee_ratio, maker_fee_ratio, slot, nonce, client_oid=""):
     assert side in ("BUY", "SELL")
     assert time_in_force in ("GTC", "IOC", "FOK", "GTX")
@@ -99,14 +99,14 @@ def place_order(acct_id, product, side, time_in_force, price, size,
     zksigner = sdk.ZkLinkSigner.new_from_hex_eth_signer(eth_priv_key)
 
     order = sdk.Order(
-        account_id=acct_id,
-        sub_account_id=1,
+        account_id=account_id,
+        sub_account_id=21,
         slot_id=slot,
         nonce=nonce,
         base_token_id=product.get('l2baseCurrencyId'),
         quote_token_id=product.get('l2quoteCurrencyId'),
-        amount=str(int(price * (10 ** 18))),
-        price=str(int(size * (10 ** 18))),
+        amount=str(int(size * (10 ** 18))),
+        price=str(int(price * (10 ** 18))),
         is_sell=side == "BUY",
         has_subsidy=False,
         maker_fee_rate=maker_fee_ratio,
@@ -114,6 +114,8 @@ def place_order(acct_id, product, side, time_in_force, price, size,
         signature=None,
     )
     signed_order = order.create_signed_order(zksigner)
+    print(signed_order.is_valid())
+    print(signed_order.is_signature_valid())
     order_signature = signed_order.get_signature();
 
     args = {
@@ -289,17 +291,36 @@ def get_product_by_id(product_id):
 
 
 if __name__ == "__main__":
-    server_info()
-    products_info()
-    eth_usd_product = get_product_by_id('wETH-USD')
-    get_jwt_token()
+    # print(server_info())
+    # resp = accounts_info()
+    # print(resp)
+    # products_info()
+    # products_info()
+    eth_usdt_product = get_product_by_id('wETH-USDT')
+    # get_jwt_token()
     self_info = get_self_info()
     slots = get_slot_batchly(5)
-    order = place_order(self_info.get('l2userId'), eth_usd_product, "BUY", "GTC", 300.0, 1.0, 10, 5, slots[0]["slot"],
-                        slots[0]["nonce"])
-    print(order.get('id'))
-    cancel_order("wETH-USD", order.get('id'))
-    list_orders("wETH-USD", 0, int(time.time()), 10)
-    get_order("wETH-USD", order.get('id'))
-    list_open_orders("wETH-USD", 10)
-    accounts_info()
+    slot_id = slots[0]["slot"]
+    nonce = slots[0]["nonce"]
+    maker_fee_rate = 30
+    taker_fee_rate = 20
+    data = {
+        "account_id": self_info.get("l2userId"),
+        "product": eth_usdt_product,
+        "side": "BUY",
+        "time_in_force": "GTC",
+        "price": 2614.2,
+        "size": 1.0,
+        "taker_fee_ratio": 20,
+        "maker_fee_ratio": 30,
+        "slot": 0,
+        "nonce": 0,
+        "client_oid": "",
+    }
+    order = place_order(**data)
+    # print(order.get('id'))
+    # cancel_order("wETH-USD", order.get('id'))
+    # list_orders("wETH-USD", 0, int(time.time()), 10)
+    # get_order("wETH-USD", order.get('id'))
+    # list_open_orders("wETH-USD", 10)
+    # accounts_info()
